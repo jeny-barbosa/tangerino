6713b267dd1c374ef771f13a3f6af10cb13b2689
Jennefer <jeny-barbosa@areacentral.github.com>
2019-07-22 15:37:30 -0300

    Arquivos iniciais

---
+++ background.js
@@ -0,0 +1,52 @@
+var botao = document.getElementById('botao');
+
+botao.addEventListener("botao", fnClickMe);
+
+function fnClickMe() {
+  oTabela = document.createElement('table');
+  aData = [];
+  console.log(aData);
+  $('input[name^="container:listaPontosContainer"]').each(function (index, el) {
+    var oTr = $(el).closest('tr');
+    var oTable = $(oTr).closest('table');
+    var oTrData = '';
+    var bSairForeach = false;
+    $(oTable).find('tr').each(function (index2, el) {
+      if ($(el).hasClass('folhaponto-header') && !bSairForeach) { 
+        oTrData = el;
+      }
+      if ($(oTr).index() == $(el).index()) {
+        bSairForeach = true;
+      }
+    });
+    var sPontoHora = $(el).val();
+    var sPontoDia = $('td > span', oTrData).get(1).innerHTML;
+    oTrTabela = document.createElement('tr');
+    oTdTabela1 = document.createElement('td');
+    oTdTabela1.innerHTML = sPontoDia;
+    oTdTabela2 = document.createElement('td');
+    oTdTabela2.innerHTML = sPontoHora;
+    oTrTabela.appendChild(oTdTabela1);
+    oTrTabela.appendChild(oTdTabela2);
+    oTabela.appendChild(oTrTabela);
+    aData.push([{v: sPontoDia, t: 's'}, {v: sPontoHora, t: 's'}]);
+  });
+  $('#minha-tabela-teste').detach();
+  oTabela.id = 'minha-tabela-teste';
+  document.body.appendChild(oTabela);
+  var oTableXlsx2 = $('#minha-tabela-teste').tableExport({
+    formats: ['xlsx'],
+    exportButtons: false
+  });
+  console.log(aData);
+  oTableXlsx2.export2file(aData, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'relatorio2', ' xlsx');
+  function awesomeTask() {
+    totallyAwesome();
+  }
+  function clickHandler(e) {
+    setTimeout(awesomeTask, 1000);
+  }
+}
+
+
+
---
+++ script.js
@@ -0,0 +1,19 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://vultr.areacentral.com.br/401/jquery/export-to-xlsx/xlsx.core.min.js';
+document.head.appendChild(script);
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://vultr.areacentral.com.br/401/jquery/export-to-xlsx/Blob.min.js';
+document.head.appendChild(script);
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://vultr.areacentral.com.br/401/jquery/export-to-xlsx/FileSaver.min.js';
+document.head.appendChild(script);
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://vultr.areacentral.com.br/401/jquery/export-to-xlsx/tableexport.min.js';
+document.head.appendChild(script);
